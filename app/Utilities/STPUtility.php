fa8b0f057fa4fe371c6a199f3a818002eed05be3
Abel Hernandez <abel.hernandez@witenconsulting.com>
2023-09-11 08:40:06 -0600

    adecuacion en creacion de digito verificador y Clave Rastreo

--- app/Utilities/STPUtility.php
+++ app/Utilities/STPUtility.php
@@ -15,8 +15,20 @@ class STPUtility
     public static function generateAccountNumber(Model $model): string
     {
         $clientNumber = str_pad($model->id,7,"0", STR_PAD_LEFT);
+        $accountNumber = "6463203687{$clientNumber}";
+        $verifiedDigit = (new STPUtility())->getVerifiedDigit($accountNumber);
+        return "$accountNumber$verifiedDigit";
+    }
+
+    public static function generateAccountNumber2($id): string
+    {
+        $clientNumber = str_pad($id,7,"0", STR_PAD_LEFT);
         $accountNumber = "6461803687{$clientNumber}";
         $verifiedDigit = (new STPUtility())->getVerifiedDigit($accountNumber);
+//        strlen($verifiedDigit) > 1 && dd_json([
+//            $accountNumber,
+//            $verifiedDigit
+//        ]);
         return "$accountNumber$verifiedDigit";
     }
 
@@ -29,17 +41,18 @@ class STPUtility
         $result = collect(str_split($accountNumber))->map(callback: function ($value, $index) {
             $first = array(1,4,7,10,13,16);
             $second = array(2,5,8,11,14,17);
-            $third = array(3,6,9,12,15);
+            //$third = array(3,6,9,12,15);
             $index = $index + 1;
             $value = intval($value);
             return match (true) {
                 in_array($index, $first) == true => ($value * 3) % 10,
                 in_array($index, $second) == true => ($value * 7) % 10,
-                in_array($index, $third) == true => ($value) % 10,
-                default => 0,
+                default => ($value) % 10,
             };
         });
-        return (10 - (collect($result)->sum() % 10) % 10);
+        //10 % (10 - (collect($result)->sum() % 10))
+        //(10 - (collect($result)->sum() % 10) % 10)
+        return 10 % (10 - (collect($result)->sum() % 10));
     }
 
     public static function sign(array $data)
